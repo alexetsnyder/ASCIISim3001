fe9a3c5bc44d0d0aac369c28132056f00f46bd23
Alex E.T. Snyder <alexetsnyder@gmail.com>
2025-01-04 10:20:10 -0500

    SFML Hello World
    Added SFML library and created a basic hello world that draws a circle to a window.

--- TextSim3001/Main.cpp
+++ TextSim3001/Main.cpp
@@ -1,9 +1,32 @@
+#include <SFML/Graphics.hpp>
+
 #include <cstdlib>
 #include <iostream>
 
 int main(int argv, char** argc)
 {
 	std::cout << "Hello World!\n";
+
+	sf::RenderWindow window{ sf::VideoMode{ { 200, 200 } }, "SFML Hello World!"};
+	sf::CircleShape circle{ 100.0f };
+	circle.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		while (const auto event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(circle);
+		window.display();
+	}
+
+	std::cout << "Window Closed!\n";
 
 	return EXIT_SUCCESS;
 }
