d2c96e8d1bdd517b1f330dd4e3f05271581786a1
Alex E.T. Snyder <alexetsnyder@gmail.com>
2025-07-04 11:28:52 -0400

    Player And World
    Draws a grid of blue tilde and then draws a red player character on top.

--- ASCIISim3001/Main.cpp
+++ ASCIISim3001/Main.cpp
@@ -5,39 +5,35 @@
 #include <cstdlib>
 #include <iostream>
 
+void createWorld(int width, int height, sf::Sprite& glyph_set, sf::RenderTexture& world);
+
 int main(int argv, char** argc)
 {
 	sf::RenderWindow window{ sf::VideoMode{ { 800, 600 } }, "SFML Hello World!" };
 
-	sf::Texture glyph_texture{};
-	if (!glyph_texture.loadFromFile("Assets/Textures/Haowan_Curses_1440x450.png"))
+	sf::Texture glyph_set_texture{};
+	if (!glyph_set_texture.loadFromFile("Assets/Textures/Haowan_Curses_1440x450.png"))
 	{
 		std::cout << "Error loading texture.\n";
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
-	sf::Sprite glyph_sprite{ glyph_texture };
-
-	glyph_sprite.setTextureRect(Glyphs::DWARF_FACE1);
+	sf::Sprite glyph_set{ glyph_set_texture };
 
 	sf::RenderTexture render_texture{ { 288, 288 } };
 
-	render_texture.clear();
-	for (int i = 0; i < 288; i += 18)
-	{
-		for (int j = 0; j < 288; j += 18)
-		{
-			glyph_sprite.setPosition(sf::Vector2f{ (float)i, (float)j });
-			render_texture.draw(glyph_sprite);
-		}
-	}
-	render_texture.display();
+	createWorld(288, 288, glyph_set, render_texture);
 
 	sf::Texture display_texture{ render_texture.getTexture() };
 	sf::Sprite display_sprite{ display_texture };
 
 	sf::FloatRect bounds{ display_sprite.getGlobalBounds() };
 	display_sprite.setPosition({ 400.0f - bounds.size.x / 2, 300.0f - bounds.size.y / 2 });	
+	sf::Vector2f worldPos{ display_sprite.getPosition() };
+
+	glyph_set.setTextureRect(Glyphs::DWARF_FACE1);
+	glyph_set.setPosition({ worldPos.x + 288 / 2.0f, worldPos.y + 288 / 2.0f });
+	glyph_set.setColor({ 255, 0, 0 });
 
 	while (window.isOpen())
 	{
@@ -51,10 +47,28 @@ int main(int argv, char** argc)
 
 		window.clear();
 		window.draw(display_sprite);
+		window.draw(glyph_set);
 		window.display();
 	}
 
 	std::cout << "Window Closed!\n";
 
 	return EXIT_SUCCESS;
+}
+
+void createWorld(int width, int height, sf::Sprite& glyph_set, sf::RenderTexture& world)
+{
+	glyph_set.setTextureRect(Glyphs::TILDE);
+	glyph_set.setColor({ 0, 0, 255 });
+
+	world.clear();
+	for (int i = 0; i < height; i += 18)
+	{
+		for (int j = 0; j < width; j += 18)
+		{
+			glyph_set.setPosition(sf::Vector2f{ (float)i, (float)j });
+			world.draw(glyph_set);
+		}
+	}
+	world.display();
 }
--- ASCIISim3001/Monolith/Rendering/Glyphs.h
+++ ASCIISim3001/Monolith/Rendering/Glyphs.h
@@ -6,6 +6,7 @@ class Glyphs
 {
 	public:
 		static constexpr sf::IntRect DWARF_FACE1{ { 18, 0 }, { 18, 18} };
+		static constexpr sf::IntRect TILDE{ { 252, 126 }, { 18, 18} };
 
 	private:
 };
